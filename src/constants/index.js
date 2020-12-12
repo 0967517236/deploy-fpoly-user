edca6f15098a9456f058e486e94f068619deb64a
0967517236 <thajnguyen165@gmail.com>
2020-12-12 15:20:59 +0700

    update

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,4 +3,4 @@ export const ACCESS_TOKEN = 'accessToken';
 
 export const OAUTH2_REDIRECT_URI = 'https://fpolyfoodapp.netlify.app/oauth2/redirect'
 
-export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
+export const GOOGLE_AUTH_URL = 'http://5084ac2ac30b.ngrok.io/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
